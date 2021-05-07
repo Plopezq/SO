e010b210805bf32dee072da0437380818625d9fa
Pablo Lopez <lopezmartinpablo@gmail.com>
2021-05-07 11:11:28 +0200

    Anyadidos los comentarios de clase

--- FicherosP3/Projects/Simulator/simulator.c
+++ FicherosP3/Projects/Simulator/simulator.c
@@ -11,16 +11,18 @@ int estado = EN_RUTA;
 int parada_actual = 0; // parada en la que se encuentra el autobus 
 int n_ocupantes = 0; // ocupantes que tiene el autobus
 
-// personas que desean subir en cada parada
+// personas que desean subir en cada parada --> array
 int esperando_parada[N_PARADAS]; //= {0,0,...0};
 
-// personas que desean bajar en cada parada
+// personas que desean bajar en cada parada --> array
 int esperando_bajar[N_PARADAS]; //= {0,0,...0};
 
 // Otras definiciones globales (comunicacion y sincronizacion)
 
+//Ejemplo: distintos MUTEX y VARIABLES DE CONDICION
+
 void * thread_autobus(void * args) { 
-	while (/* condicion */) { 
+	while (/* condicion */) { //Puede ser infinito, la ruta es circular
 		// esperar a que los viajeros suban y bajen 
 		Autobus_En_Parada(); 
 		// conducir hasta siguiente parada 
@@ -53,11 +55,40 @@ void Autobus_En_Parada(){
 	/* Ajustar el estado y bloquear al autobus hasta que no haya pasajeros que 
 	quieran bajar y/o subir la parada actual. Despues se pone en marcha */
 
-	
+	while(esperando_parada[parada_actual]){
+		broadcast(	);
+		wait(	);
+	}
+
+
+
+
 }
 
 void Conducir_Hasta_Siguiente_Parada(){ 
 	/* Establecer un Retardo que simule el trayecto y actualizar numero de parada */ 
+	//Generar numero aleatorio para un sleep
+
+	//INICIO SECCION CRITICA
+		estado = EN_RUTA; //V
+	//FINAL SECCION CRITICA
+
+
+	sleep(1/*tiempo_random*/); //CONDUCIENDO
+
+
+	//INICIO SECCION CRITICA
+	//Siguiente parada
+	if(parada_actual < N_PARADAS -1)// Paradas 0 1 2 3 
+		parada_actual +=1; //V
+	else { //Esta en la ultima parada - Parada 4
+		parada_actual = 0; //V
+	}
+	estado = EN_PARADA;
+	//FINAL SECCION CRITICA
+
+
+
 
 
 }
@@ -67,7 +98,24 @@ void Subir_Autobus(int id_usuario, int origen){
 	el autobus se pare en dicha parada y subira. El id_usuario puede utilizarse para 
 	proporcionar informacion de depuracion */ 
 
+	//INICIO SECCION CRITICA
+	esperando_parada[origen]++;
+	//FINAL SECCION CRITICA
 
+	while((estado)!= EN_PARADA
+			|| (parada_actual) != origen){
+
+		condWait( , );
+
+	}
+	// ----------------------------
+	//INICIO SECCION CRITICA
+	esperando_parada[origen]--;
+	//FINAL SECCION CRITICA
+
+	if(){ //SI SOY EL ULTIMO
+		signal(		);
+	}
 }
 
 void Bajar_Autobus(int id_usuario, int destino){ 
